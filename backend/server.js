9dd394558721b464402deae607341015a1b56989
GeorgePag4028 <georgepag4028@gmail.com>
2020-12-31 17:57:49 +0200

    Create second entities

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,11 @@ const app = express();
 //this takes the createdb route and use this when we use /createdb 
 app.use('/createdb',require('./routes/createdb'));
 
-//this adds the table of the users
-app.use('/createUserTable',require('./routes/createUserTable'));
 
-//this adds the table of the car
-app.use('/createCarTable',require('./routes/createCarTable'));
 
-app.use('createUserHasCarTable', require('./routes/createUserHasCarTable'));
 
-app.use('createCarChargedTransactionTable',require('./routes/createCarChargedTransactionTable'));
 
-app.use('createStationOffersDiscountChargerTable',require('./routes/createStationOffersDiscountChargerTable'));
+
 
 //app.use('/api/members',require('./routes/api/members'));
 const PORT = process.env.PORT || 5000;
